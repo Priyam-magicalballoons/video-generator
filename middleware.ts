0aeb306b333ab1f0fce04c94a5b85dfb7995ba46
Priyam-magicalballoons <magicalballoons.priyam@gmail.com>
2025-05-17 18:20:35 +0530

    ibnitial

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,18 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  // const { pathname } = request.nextUrl;
-  // const token = request.cookies.get('user')?.value;
+  const { pathname } = request.nextUrl;
+  const token = request.cookies.get('user')?.value;
 
-  // if (pathname === '/login' && token) {
-  //   return NextResponse.redirect(new URL('/dashboard', request.url));
-  // }
+  if (pathname === '/login' && token) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
 
-  // const protectedRoutes = ['/', '/i-pledge', '/record-video',"/doctors","/dashboard"]; 
-  // const isProtected = protectedRoutes.includes(pathname)
+  const protectedRoutes = ['/', '/i-pledge', '/record-video',"/doctors","/dashboard"]; 
+  const isProtected = protectedRoutes.includes(pathname)
 
-  // if (isProtected && !token) {
-  //   return NextResponse.redirect(new URL('/login', request.url));
-  // }
-  // return NextResponse.next();
+  if (isProtected && !token) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  return NextResponse.next();
 }
