db68e7587971dbbb12c3f11f8d43c637e2858060
Priyam-magicalballoons <magicalballoons.priyam@gmail.com>
2025-05-18 19:17:08 +0530

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -12,7 +12,7 @@ const Card = ({text,icon,onclick}:CardProps) => {
   return (
     <div className='flex items-center w-full' onClick={onclick}>
       <div className='w-[40%] flex items-center justify-start pl-5'>
-        <img src={icon} alt={text} height={70} width={70} className=' border-primary border rounded-full'/>
+        <img src={icon} alt={text} height={70} width={70} className=' border-primary border rounded-full p-1'/>
       </div>
       <div className='w-[60%] flex justify-start text-primary font-semibold group-hover:text-white group'>
         <p>{text}</p>
